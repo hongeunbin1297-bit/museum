55a35d714a6bdcffcdd071157cf4b2430b6c9add
eunbin <hongeunbin1297@gmail.com>
2025-08-26 16:12:08 +0900

    second commit

--- js/museum.js
+++ js/museum.js
@@ -1,6 +1,30 @@
+$(function(){
     $('.gotop').click(function (e) {
         e.preventDefault()
         $('html,body').stop().animate({
             scrollTop: 0
         }, 1000)
     })
+
+
+    $(".hamburger").click(function(){
+        $(".main").toggleClass("on")
+        if($('.main').hasClass('on')){
+            $(".main").fadeOut()
+        }else{
+            $(".main").fadeIn()
+
+        }
+    })
+
+    $(".main>li").click(function(){
+        $('.main li').addClass('on')
+        $(this).removeClass('on')
+        $('.main>li ul').removeClass('on')
+        $(this).find("ul").addClass('on')
+    })
+
+    
+
+})    
+
